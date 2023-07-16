f667c223ad97044c5fcd8db6b644e768d96ed1ec
Jade Stewart <jadekstewart3@gmail.com>
2023-07-16 11:06:04 -0600

    Initial commit

---
+++ app/frontend/entrypoints/users.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const container = document.getElementById('users');
+const root = ReactDOM.createRoot(container);
+
+root.render(<h1>hi I'm Paul</h1>);
