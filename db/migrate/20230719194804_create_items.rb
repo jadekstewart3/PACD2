c410dbd9751fb07690ab728b9a27dc64cb3c1363
Jade Stewart <jadekstewart3@gmail.com>
2023-07-19 14:51:23 -0600

    Create PACD database

---
+++ db/migrate/20230719194804_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :quantity
+      t.text :note
+      t.string :assignee
+
+      t.timestamps
+    end
+  end
+end
