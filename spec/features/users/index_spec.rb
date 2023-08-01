729be5fd23079add0ce67ce166175b22b369ca25
Jade Stewart <jadekstewart3@gmail.com>
2023-08-01 15:19:47 -0600

    Fully configure rspec and capybara to work with javascript

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "User Index", driver: :selenium_chrome, js: true do
   it "shows some text" do
     visit users_path
     Capybara.using_wait_time(10) do
-      expect(page).to have_content("Hi, i'm paul")
+      expect(page).to have_content("hi I'm Paul")
     end
   end
 end
