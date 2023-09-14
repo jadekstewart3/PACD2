6f6d77ee7d22dd721b18f80dec3362e789c8867c
Jade Stewart <jadekstewart3@gmail.com>
2023-09-14 14:57:28 -0600

    remove devise from users table

--- db/migrate/20230815170908_add_column_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnNameToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_170908) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_182506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,14 +68,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_170908) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
+    t.string "password_confirmation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "list_items", "items"
