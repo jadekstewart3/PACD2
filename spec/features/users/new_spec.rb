4ac22005591706a96b56c0ebe12c35bfd9b6d99d
Jade Stewart <jadekstewart3@gmail.com>
2023-09-14 14:56:44 -0600

    fix spelling error

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Register User", driver: :selenium_chrome, js: true do 
+  context "As a visitor" do 
+    it "when visit the root path a see a button to register as a new user" do
+    
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe User, type: :model do
   describe "Validations" do 
     it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { should validate_presence_of :email }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
     it { should validate_presence_of :password }
   end
 
